0bf427b87d055e9c5d3df74fce40ea8cdc33baf2
studio-luke <goalone4r@postech.ac.kr>
2019-11-06 03:04:26 +0900

    minor error fixes

--- embedder.py
+++ embedder.py
@@ -11,30 +11,36 @@ class Embedder:
             return super(Embedder, cls).__new__(cls)
         else:
             print("[Embedder Error] Cannot find html file on given path '{}'... Embedder not generated"\
-                .format(getcwd() + "\\\\" util.VIEW_DEFAULT[:-1] + "\\\\" + filename))
+                .format(getcwd() + "\\\\" + util.VIEW_DEFAULT[:-1] + "\\\\" + filename))
             return None
 
     def __init__(self, html_filename):
         self.filename = html_filename
         self.DomTree = html.parse(util.VIEW_DEFAULT + html_filename)
-        node_iterator = self.DomTree.getiterator()
-        self.asList = list(node_iterator)
         self._assign_id()
 
     def _assign_id(self):
         id_inc = -1
-        for node in self.asList:
+        for node in self.iter():
             if (type(node) == html.HtmlComment):
                 continue
             id_inc += 1
             node_id = hex(id_inc)[2:]    #[2:] : to exclude '0x'
             # hex(int): return hexadecimal format of the num as string type
 
-            node.set(PREFIX+"_id", node_id)
+            node.set(util.PREFIX+"_id", node_id)
 
-    def find_node(node_id):
-        pass
+    def iter(self, onlyElement=True):
+        if onlyElement:
+            return self.DomTree.getiterator(tag=etree.Element)
+        return self.DomTree.getiterator()
 
+
+    def find_node(self, node_id):
+        for node in self.iter():
+            if node.attrib["node_id"] == node_id:
+                return node
+        return None
 
 
 
