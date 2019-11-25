ab70e175ddbb1e122dde53d30f4c0723d6d9e92c
studio-luke <goalone4r@postech.ac.kr>
2019-11-26 04:09:40 +0900

    some new databases & inspector updated

--- inspector.py
+++ inspector.py
@@ -3,28 +3,32 @@ from json import load as json_load
 import sys
 import util
 
-def highlighter(html_file, json_file):
+def highlighter(json_file):
 
     # assert html_file[-5:].lower() == 'html', "HTML filename must ends with '.html'"
     # assert json_file[-5:].lower() == 'json', "JSON filename must ends with '.html'"
 
     # load HTML as ElementTree, load JSON as dictionary obj
-    tree= html.parse(util.VIEW_DEFAULT + html_file)
+    
     with open(json_file, "r") as f:
         json_data = json_load(f)
 
     # Mark the value data
-    for value in json_data["pair"]:
-        ele_val = tree.xpath(value["xpath"])
-        ele_val[0].set("style", "background-color:skyblue")
+    for site in json_data:
+        html_file = site["node"] + ".html"
+        tree= html.parse(util.VIEW_DEFAULT + html_file)
+        
+        for data in site["element_attrs"].keys():
+	        ele_val = tree.xpath(data["value_xpath"])
+	        ele_val[0].set("style", "background-color:skyblue")
 
-    # Mark the attribute data
-    attrib = value["attribute"]
-    if attrib["xpath"] != "[None]":
-        ele_attr = tree.xpath(attrib["xpath"])
-        ele_attr[0].set("style", "background-color:magenta")
+		    # Mark the attribute data
+		    
+		    if data["attr_xpath"] != "[NONE]":
+		        ele_attr = tree.xpath(data["attr_xpath"])
+		        ele_attr[0].set("style", "background-color:magenta")
 
-    tree.write_c14n(util.VIEW_DEFAULT+ html_file[:-5] + "_highlighted.html")
+	    tree.write_c14n(util.VIEW_DEFAULT+ "highlited/" + html_file[:-5] + "_hl.html")
 
     # try:
     # except:
